ead9a7305fc57acedf0079f58d024252db93e362
ChanduKReddy99 <chanduk.amical@gmail.com>
2022-09-08 09:33:02 +0530

    flask app and html template created

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template 
+import numpy as np
+
+app= Flask(__name__)
+
+# loading the model
+scaling= pickle.load(open('scaling.pkl', 'rb'))
+reg_model= pickle.load(open('linearReg.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods= ['POST'])
+def predict_api():
+    data= request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data= scaling.transform(np.array(list(data.values())).reshape(1,-1))
+    output= reg_model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
